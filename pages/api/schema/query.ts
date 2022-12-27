d2d629212fe1b9c7b554d299a219d7d6dfa3f0d7
MrCocoNuat <aarontwang@gmail.com>
2022-12-27 17:49:00 -0500

    saner schemabuild, allow null result list elements

--- pages/api/schema/query.ts
+++ pages/api/schema/query.ts
@@ -2,36 +2,38 @@ import { heroDao, skillDao } from "../../../dao/dao-registry";
 import { SkillDefinitionObject, HeroDefinitionObject } from "./object";
 import { builder } from "./schema-builder";
 
-export const setQueries =
-builder.queryType({
-    fields: (t) => ({
-        
-        // define queries here. This one returns a field (with subfields)
-        skills: t.field({
-            // its args are a
-            args: {
-                // [Int!]! named idNums
-                idNums: t.arg({
-                    type: ["Int"],
-                    required: true,
-                }),
-            },
-            // and the return field is a Graphql Object type implemented elsewhere. To get its backing object,
-            type: [SkillDefinitionObject],
-            // run this resolver given args.
-            resolve: async (parent, {idNums}) => (await skillDao.getByIdNums(idNums))
-        }),
-        
-        heroes: t.field({
-            args: {
-                idNums: t.arg({
-                    type: ["Int"],
-                    required: true
-                }),
-            },
-            type: [HeroDefinitionObject],
-            resolve: async (parent, {idNums}) => (await heroDao.getByIdNums(idNums))
-        })
-    }),
+
+export const setQueries = () => {
+    builder.queryType();
+
+    // define queries here. This one returns a field (with subfields)
+    builder.queryField("skills", (qfb) => qfb.field({
+        description: "Looks up a list of idNums and returns a SkillDefinition for each",
+        // its args are a
+        args: {
+            // [Int!]! named idNums
+            idNums: qfb.arg({
+                type: qfb.arg.listRef("Int", {required: true}),
+                required: true,
+            }),
+        },
+        // and the return field is a [Graphql Object type implemented elsewhere]!. To get its backing object,
+        type: qfb.listRef(SkillDefinitionObject, {nullable: true}),
+        nullable: false,
+        // run this resolver given args.
+        resolve: async (parent, {idNums}) => (await skillDao.getByIdNums(idNums))
+    }));
     
-});
+    builder.queryField("heroes", (qfb) => qfb.field({
+        description: "Looks up a list of idNums and returns a HeroDefinition for each",
+        args: {
+            idNums: qfb.arg({
+                type: qfb.arg.listRef("Int", {required: true}),
+                required: true
+            }),
+        },
+        type: qfb.listRef(HeroDefinitionObject, {nullable: true}),
+        nullable: false,
+        resolve: async (parent, {idNums}) => (await heroDao.getByIdNums(idNums))
+    }))
+}
--- pages/api/schema/schema.ts
+++ pages/api/schema/schema.ts
@@ -1,7 +1,8 @@
+import { setMutations } from "./mutation";
 import { setQueries } from "./query";
 import { builder } from "./schema-builder";
 
-setQueries; //TODO- this is a REALLY messed up way to ensure that the query type is set. is there a better one?
-//setMutations; // No mutations
+setQueries(); 
+setMutations(); // No mutations
 
 export const schema = builder.toSchema();

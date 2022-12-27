3498ee4b4ccca52fd8748f40fa6e38323fb84e52
MrCocoNuat <aarontwang@gmail.com>
2022-12-27 15:37:57 -0500

    oops include installing pothos too

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,29 +1,7 @@
 import { createYoga, createSchema } from 'graphql-yoga'
-
-const typeDefs = /* GraphQL */ `
-  type Query {
-    users: [User!]!
-  }
-  type User {
-    name: String
-  }
-`
-
-const resolvers = {
-  Query: {
-    users() {
-      return [{ name: 'Nextjs' }]
-    },
-  },
-}
-
-const schema = createSchema({
-  typeDefs,
-  resolvers,
-})
+import schema from './graphql-schema'
 
 export default createYoga({
   schema,
-  // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
   graphqlEndpoint: '/api/graphql',
 })

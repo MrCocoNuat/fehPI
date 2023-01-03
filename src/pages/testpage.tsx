f6187b49213a12817ec797121af5eab86b5601eb
MrCocoNuat <aarontwang@gmail.com>
2023-01-03 18:35:35 -0500

    #37 revert changes to header

--- src/pages/testpage.tsx
+++ src/pages/testpage.tsx
@@ -10,7 +10,7 @@ export default function TestComponent(props: { user: any }) {
     const { loading, data, error } = useQuery(PING);
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
-    const {theme, setTheme} = useTheme();
+    const { theme, setTheme } = useTheme();
 
     console.log(`Check console for "SENTINEL" strings, leaking server-side information`);
     console.log(`graphql reply: ${JSON.stringify(data)}`);
@@ -20,26 +20,26 @@ export default function TestComponent(props: { user: any }) {
             <title>Turnwheel</title>
             <meta name="description" content="turnwheel" />
         </Head>
-        <div className="h-max">
-            <nav className="bg-red-900 text-white h-[50px] w-full sticky top-0 flex justify-between items-center z-50">
-                <div>logo</div>
-                <div>name</div>
-                <div className="flex">
-                    <div onClick={
-                        () => {
-                            setTheme((theme === "dark")? "light" : "dark");
-                        }
-                    }>dark</div>
-                    <div>lang</div>
-                </div>
-            </nav>
 
-            <main className="dark:bg-black dark:text-white">
-                <div className="flex justify-center">
-                    <BattlePane></BattlePane>
-                </div>
-            </main>
-        </div>
+        <nav className="bg-red-900 text-white h-[50px] w-full fixed top-0 flex justify-between items-center z-50">
+            <div>logo</div>
+            <div>name</div>
+            <div className="flex">
+                <div onClick={
+                    () => {
+                        setTheme((theme === "dark") ? "light" : "dark");
+                    }
+                }>dark</div>
+                <div>lang</div>
+            </div>
+        </nav>
+
+        <main className="dark:bg-black dark:text-white mt-[50px]">
+            <div className="flex justify-center">
+                <BattlePane></BattlePane>
+            </div>
+        </main>
+
     </>
 }
 

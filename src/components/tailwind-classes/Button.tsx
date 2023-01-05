10cec78f972747614c3538df6d489f57be615217
MrCocoNuat <aarontwang@gmail.com>
2023-01-04 21:29:44 -0500

    #37 adjust css values

--- src/components/tailwind-classes/Button.tsx
+++ src/components/tailwind-classes/Button.tsx
@@ -4,23 +4,19 @@ export function Button({
     onClick,
     value,
     className,
-    padding,
 }: {
     onClick: MouseEventHandler,
     value: JSX.Element | string,
     className?: String,
-    padding?: { x: number, y: number }
 }) {
-    className = className ?? "";
-    padding = padding ?? { x: 0, y: 0 };
+    const moreClassName = className ?? "";
 
     return <button
         className={`rounded-full
         bg-blue-800 hover:bg-blue-900 active:bg-blue-700 
         dark:bg-purple-700 dark:hover:bg-purple-900 dark:active:bg-purple-500
         text-white
-        px-${padding.x} py-${padding.y}
-        ${className}`}
+        ${moreClassName}`}
         onClick={onClick}>
         {value}
     </button>

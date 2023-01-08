6bb287730be00ff38a162a49ede7118d512d7278
MrCocoNuat <aarontwang@gmail.com>
2023-01-08 10:45:45 -0500

    #19 enable server api cache - data readonly anyway

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,7 +1,14 @@
+import { useResponseCache } from '@graphql-yoga/plugin-response-cache'
 import { createYoga } from 'graphql-yoga'
 import { schema } from './schema/schema'
 
 export default createYoga({
   schema,
   graphqlEndpoint: '/api/graphql',
+  plugins: [
+    useResponseCache({
+      // for everyone
+      session: () => null,
+    })
+  ]
 })

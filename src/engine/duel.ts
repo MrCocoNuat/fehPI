00cd3b4247c08ef1391df548bc468199e36f4d13
MrCocoNuat <aarontwang@gmail.com>
2023-02-16 09:35:53 -0500

    Start Duel engine

---
+++ src/engine/duel.ts
@@ -0,0 +1,9 @@
+import { BattleContext, Combatant } from "./types";
+
+export function duel(battleContext: BattleContext, initiator: Combatant, opponent: Combatant){
+
+}
+
+function checkErrorConditions(battleContext: BattleContext, initiator: Combatant, opponent: Combatant){
+    
+}
